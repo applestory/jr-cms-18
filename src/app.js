3eb77742ed0cb7f043c9a882f3384f33da12b4d9
Dylan Lang <fullartonstory@gmail.com>
2023-03-29 17:16:03 +1030

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const unknownError = require('./middleware/error/unknownError');
-const validationError = require('./middleware/error/validationError');
-require('express-async-errors');
-const errorHandler = require('./middleware/errorHandler');
-const router = require('./routes');
+const express = require("express");
+const unknownError = require("./middleware/error/unknownError");
+const validationError = require("./middleware/error/validationError");
+require("express-async-errors");
+const errorHandler = require("./middleware/errorHandler");
+const router = require("./routes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/v1', router);
+app.use("/v1", router);
 
 app.use(validationError);
 // other error handlers
 app.use(unknownError);
 
-app.use('', (req, res) => {
-  res.send('healthy');
+app.get("", (req, res) => {
+  res.send("healthy");
 });
 
 module.exports = app;
